ab55eece31d86897fa78b8bc79eb32d20d479268
Arnav K <arnavkaushal09@gmail.com>
2024-08-25 14:14:26 +0000

    starter backend

--- frontend-app/app/channel/[channel_id]/page.tsx
+++ frontend-app/app/channel/[channel_id]/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { Bubble } from "pixel-retroui";
-import ApexCharts from 'apexcharts'
+import ApexCharts from "apexcharts";
 import ReactApexChart from "react-apexcharts";
-
 
 export default function ViewChannel({
   params,
@@ -12,65 +11,87 @@ export default function ViewChannel({
 }) {
   const data = {
     options: {
+      colors: [
+        "#400810",
+        "#630010",
+        "#990931",
+        "#f01819",
+        "#f97d6d",
+        "#ffbab5",
+        "#ffeae0",
+      ],
       chart: {
         id: `${params.channel_id}-bar`,
-        colors: ['#2E93fA', '#66DA26', '#546E7A', '#E91E63', '#FF9800']
       },
       xaxis: {
-        categories: [
-          new Date().toLocaleString()
-        ]
-      }
+        categories: [new Date().toLocaleString(), new Date().toLocaleString()],
+      },
     },
     series: [
       {
         name: "subscribers",
-        data: [45353]
-      }
-    ],  responsive: [
+        data: [45353, 232],
+      },
+    ],
+    responsive: [
       {
         breakpoint: 1000,
         options: {
           plotOptions: {
             bar: {
-              horizontal: false
-            }
+              horizontal: false,
+            },
           },
           legend: {
-            position: "bottom"
-          }
-        }
-      }
-    ]
+            position: "bottom",
+          },
+        },
+      },
+    ],
   };
-
 
   return (
     <>
       <div>
-        <h1 className="text-xl md:text-6xl uppercase text-[#400810] font-bold">@{params.channel_id}</h1>
+        <h1 className="text-xl md:text-6xl uppercase text-[#400810] font-bold">
+          @{params.channel_id}
+        </h1>
         <h1 className="text-sm text-red-600/60 font-semibold">22323243dsfsd</h1>
-        <h1 className="my-10 text-white px-4 py-5 bg-gradient-to-br from-[#990931] to-[#f01819] text-2xl md:text-7xl tracking-tighter" style={{
-          fontWeight: 1000
-        }}>232343</h1>
+        <h1
+          className="my-10 text-white px-4 py-5 bg-gradient-to-br from-[#990931] to-[#f01819] text-2xl md:text-7xl tracking-tighter"
+          style={{
+            fontWeight: 1000,
+          }}
+        >
+          232343
+        </h1>
       </div>
 
       <div className="hiddenScrollbar max-w-[90vw] block mx-auto">
-        <ReactApexChart options={data.options}
+        <ReactApexChart
+          options={data.options}
           series={data.series}
           type="line"
-          width="500" />
+          width="500"
+        />
       </div>
 
       <div>
         <Bubble
-          borderColor="#400810" bg="#f5d8cb" textColor="#f01819" className="bg-[#f5d8cb] block mx-auto cursor-default md:text-5xl font-bold leading-relaxed" direction="left">
+          borderColor="#400810"
+          bg="#f5d8cb"
+          textColor="#f01819"
+          className="bg-[#f5d8cb] block mx-auto cursor-default md:text-5xl font-bold leading-relaxed"
+          direction="left"
+        >
           Total Videos: <strong className="text-[#630010]">2323</strong> <br />
-          Total Subscribers: <strong className="text-[#630010]">2323</strong> <br />
+          Total Subscribers: <strong className="text-[#630010]">
+            2323
+          </strong>{" "}
+          <br />
           Total Views: <strong className="text-[#630010]">2323</strong> <br />
         </Bubble>
       </div>
-
     </>
   );
 }
